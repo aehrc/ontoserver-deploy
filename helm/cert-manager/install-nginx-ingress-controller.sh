3b648c291e34b42999bba651be12629424930578
Attila Edelenyi <ede_ati@hotmail.com>
2022-12-12 16:42:39 +1000

    Add tolerations to Helm install so it deploys on Spot nodes

--- helm/cert-manager/install-nginx-ingress-controller.sh
+++ helm/cert-manager/install-nginx-ingress-controller.sh
@@ -2,4 +2,8 @@
 
 helm repo add nginx-stable https://helm.nginx.com/stable
 helm repo update
-helm install nginx-ingress-controller nginx-stable/nginx-ingress -n nginx-ingress-controller --create-namespace
+helm upgrade --install nginx-ingress-controller nginx-stable/nginx-ingress -n nginx-ingress-controller --create-namespace \
+  --set controller.tolerations[0].key="kubernetes.azure.com/scalesetpriority" \
+  --set controller.tolerations[0].operator="Equal" \
+  --set controller.tolerations[0].value="spot" \
+  --set controller.tolerations[0].effect="NoSchedule"
